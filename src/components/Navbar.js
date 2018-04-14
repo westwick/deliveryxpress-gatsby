f7062a02c6c6946068e83d0f3bd0455539966ea4
Andrew Westwick <andrewwestwick@iheartmedia.com>
2018-04-14 12:02:37 -0500

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => (
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
-          About
+          Aboutz
         </Link>
         <Link className="navbar-item" to="/products">
           Products
