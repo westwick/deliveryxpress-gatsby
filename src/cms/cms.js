5eee0f588c78aa6db2927124de990d535fc4f748
Andrew Westwick <awestwick@gmail.com>
2018-04-15 16:09:30 -0500

    remove products page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,10 @@ import CMS from 'netlify-cms'
 
 import CustomersPagePreview from './preview-templates/CustomersPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+//import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('customers', CustomersPagePreview)
 CMS.registerPreviewTemplate('businesses', CustomersPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+// CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
