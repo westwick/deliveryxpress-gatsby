ed80e0e1a0099d087d30b1a9252eab8d407fb516
Andrew Westwick <awestwick@gmail.com>
2018-04-15 10:56:16 -0500

    testing

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,14 +10,12 @@ const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet>
       <title>DeliveryXpress</title>
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous" />
       <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
       <link rel="manifest" href="/img/favicon/site.webmanifest" />
       <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#5bbad5" />
       <link rel="shortcut icon" href="/img/favicon/favicon.ico" />
-      <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="msapplication-config" content="/img/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#f8f8f8" />
@@ -25,6 +23,29 @@ const TemplateWrapper = ({ children }) => (
     <Navbar />
     <div className="app-main">{children()}</div>
     <Footer />
+    <noscript id="deferred-styles">
+      <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" />
+    </noscript>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          alert('wtf')
+          var loadDeferredStyles = function() {
+          console.log('loaded')
+          var addStylesNode = document.getElementById("deferred-styles");
+          var replacement = document.createElement("div");
+          replacement.innerHTML = addStylesNode.textContent;
+          document.body.appendChild(replacement)
+          addStylesNode.parentElement.removeChild(addStylesNode);
+          };
+          var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+          if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+          else window.addEventListener('load', loadDeferredStyles);
+            `,
+      }}
+    />
   </div>
 )
 
