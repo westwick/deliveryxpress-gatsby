d0fed8a1f1a4823e495250f4ba695125c6eefa2c
Andrew Westwick <awestwick@gmail.com>
2018-04-23 21:37:21 -0500

    sick header

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link'
 // import AOS from 'aos'
 import 'aos/dist/aos.css'
 import logoFull from '../img/dx-full.png'
+import Img from 'gatsby-image'
 
 export default class IndexPage extends React.Component {
   componentDidMount() {
@@ -13,10 +14,15 @@ export default class IndexPage extends React.Component {
   }
   render() {
     const { data } = this.props
+    console.log(data)
     const { edges: posts } = data.allMarkdownRemark
+    console.log(data)
     return (
       <div className="home">
         <div className="hero flex-centered" >
+          <div className="hero-img-wrap">
+            <Img sizes={data.herobg.childImageSharp.sizes} className="herobg" />
+          </div>
           <img src={logoFull} alt="DeliveryXpress Logo" className="hero-logo" />
           <h1 className="text-center">Right now delivery</h1>
           <h2 className="text-center">Anything and everything</h2>
@@ -62,6 +68,22 @@ export default class IndexPage extends React.Component {
               </div>
           </div>
         </section>
+        <section className="app-download section">
+          <div className="container">
+            <div className="columns">
+              <div className="column">
+                <img />
+              </div>
+              <div className="column">
+                <h2>Download our official app</h2>
+                <p>DeliveryXpress is a technology platform that connects local delivery resources with hiring clients in real-time. Delivery applications range from quick service and restaurant food to a traditional local courier.</p>
+                <div className="download-buttons">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
         <section className="whyus section">
           <div className="container">
             <h2>Why DeliveryXpress?</h2>
@@ -104,6 +126,7 @@ export default class IndexPage extends React.Component {
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
+    herobg: PropTypes.any,
     allMarkdownRemark: PropTypes.shape({
       edges: PropTypes.array,
     }),
@@ -112,6 +135,13 @@ IndexPage.propTypes = {
 
 export const pageQuery = graphql`
   query IndexQuery {
+    herobg: file(id: { regex: "/herobg/" }) {
+      childImageSharp {
+        sizes(maxWidth: 1920) {
+          ...GatsbyImageSharpSizes
+        }
+      }
+    }
     allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date] }
     ) {
